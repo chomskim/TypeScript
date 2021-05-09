81f9822bdbb6b8ea04443e72b3528105e0cceb4b
chomskim <csknova@gmail.com>
2021-05-10 08:59:13 +0900

    commit full stack react typescript

---
+++ FullStack.React.TypeScript/Chap2/any.js
@@ -0,0 +1,10 @@
+//let val: any = 22
+//val = 'string value'
+//val = new Array()
+//val.push(33)
+//console.log(val)
+var val = 22;
+val = 'string value';
+val = new Array();
+//val.doesnotexist(33)
+console.log(val);
